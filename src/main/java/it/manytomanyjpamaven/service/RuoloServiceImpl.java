a608bec7ad6f0d49e25e978e46d53a2c2465dc8d
DAVIDLEONE2001 <david.leone.2001@gmailcom>
2023-04-05 16:00:15 +0200

    Primo commit

---
+++ src/main/java/it/manytomanyjpamaven/service/RuoloServiceImpl.java
@@ -0,0 +1,126 @@
+package it.manytomanyjpamaven.service;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import it.exception.RuoloAssociatoAdUtentiException;
+import it.manytomanyjpamaven.dao.EntityManagerUtil;
+import it.manytomanyjpamaven.dao.RuoloDAO;
+import it.manytomanyjpamaven.dao.UtenteDAO;
+import it.manytomanyjpamaven.dao.UtenteDAOImpl;
+import it.manytomanyjpamaven.model.Ruolo;
+
+public class RuoloServiceImpl implements RuoloService {
+
+	private RuoloDAO ruoloDAO;
+	private UtenteDAO utenteDAO;
+
+	@Override
+	public List<Ruolo> listAll() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Ruolo caricaSingoloElemento(Long id) throws Exception {
+		// questo è come una connection
+		EntityManager entityManager = EntityManagerUtil.getEntityManager();
+
+		try {
+			// uso l'injection per il dao
+			ruoloDAO.setEntityManager(entityManager);
+
+			// eseguo quello che realmente devo fare
+			return ruoloDAO.get(id);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			EntityManagerUtil.closeEntityManager(entityManager);
+		}
+	}
+
+	@Override
+	public void aggiorna(Ruolo ruoloInstance) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void inserisciNuovo(Ruolo ruoloInstance) throws Exception {
+		// questo è come una connection
+		EntityManager entityManager = EntityManagerUtil.getEntityManager();
+
+		try {
+			// questo è come il MyConnection.getConnection()
+			entityManager.getTransaction().begin();
+
+			// uso l'injection per il dao
+			ruoloDAO.setEntityManager(entityManager);
+
+			// eseguo quello che realmente devo fare
+			ruoloDAO.insert(ruoloInstance);
+
+			entityManager.getTransaction().commit();
+		} catch (Exception e) {
+			entityManager.getTransaction().rollback();
+			e.printStackTrace();
+			throw e;
+		}
+
+	}
+
+	@Override
+	public void rimuovi(Long idRuoloToRemove) throws Exception {
+		EntityManager entityManager = EntityManagerUtil.getEntityManager();
+
+		try {
+			entityManager.getTransaction().begin();
+			ruoloDAO.setEntityManager(entityManager);
+			utenteDAO.setEntityManager(entityManager);
+			if (utenteDAO.findAllByRuolo(ruoloDAO.get(idRuoloToRemove)).size() > 0) {
+				throw new RuoloAssociatoAdUtentiException("Ruolo con utenti");
+			}
+			ruoloDAO.delete(ruoloDAO.get(idRuoloToRemove));
+			entityManager.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			entityManager.getTransaction().rollback();
+		} finally {
+			EntityManagerUtil.closeEntityManager(entityManager);
+		}
+
+	}
+
+	@Override
+	public void setRuoloDAO(RuoloDAO ruoloDAO) {
+		this.ruoloDAO = ruoloDAO;
+	}
+
+	@Override
+	public Ruolo cercaPerDescrizioneECodice(String descrizione, String codice) throws Exception {
+		// questo è come una connection
+		EntityManager entityManager = EntityManagerUtil.getEntityManager();
+
+		try {
+			// uso l'injection per il dao
+			ruoloDAO.setEntityManager(entityManager);
+
+			// eseguo quello che realmente devo fare
+			return ruoloDAO.findByDescrizioneAndCodice(descrizione, codice);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			EntityManagerUtil.closeEntityManager(entityManager);
+		}
+	}
+
+	public void setUtenteDAO(UtenteDAO utenteDAO) {
+		this.utenteDAO = utenteDAO;
+	}
+
+}
